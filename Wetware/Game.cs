7d61e8ce4870b97f53c448681eeaae74aefdd510
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-05 14:35:08 +0100

    Game: naive support for (de)serializing World

--- Wetware/Game.cs
+++ Wetware/Game.cs
@@ -1,4 +1,5 @@
 ﻿using Friflo.Engine.ECS;
+using Friflo.Engine.ECS.Serialize;
 using Friflo.Engine.ECS.Systems;
 using Wetware.Flags;
 using Wetware.Globals;
@@ -11,32 +12,49 @@ public class Game
     internal static Game Instance = null!;
     public static bool ClockTurn => Instance.World.GetUniqueEntity(EId.Clock).Tags.Has<TakingTurn>();
     
+    private readonly string _name;
     public readonly EntityStore World;
+    public bool LoadedFromFile;
     private readonly SystemRoot _updateSystems;
     private readonly SystemRoot _renderSystems;
 
-    public Game()
+    public Game(string? name)
     {
-        Instance = this;
-        World = NewWorld();
+        _name = name ?? "world";
+        
+        World = InitWorld();
         _updateSystems = new SystemRoot(World)
         {
             new EnergySystem(),
             new HealthRegenSystem(),
             new DoNothingSystem(),
         };
-        
         _renderSystems = new SystemRoot(World);
+        Instance = this;
     }
 
-    private static EntityStore NewWorld() => new() { EventRecorder = { Enabled = true } };
+    private EntityStore InitWorld()
+    {
+        var store = new EntityStore();
+        store.EventRecorder.Enabled = true;
 
-    private static UpdateTick GetTick() => new UpdateTick();
+        if (!File.Exists($"{_name}.json")) return store;
+
+        LoadedFromFile = true;
+        return store.LoadFromFile($"{_name}.json");
+    }
+
+    private static UpdateTick GetTick() => new();
     
     public void Tick()
     {
         Console.WriteLine("Ticking UpdateSystems.");
         _updateSystems.Update(GetTick());
+
+        var serializer = new EntitySerializer();
+        var writeStream = new FileStream($"{_name}.json", FileMode.Create);
+        serializer.WriteStore(World, writeStream);
+        writeStream.Close();
     }
 
     public void Render()
