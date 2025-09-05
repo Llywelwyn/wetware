c2c2dfc577315c5ecad96d9c0d2196ba5c9d968f
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-05 12:31:41 +0100

    adds "bonus" to Dice rolls, and DiceTests

---
+++ Wetware.Tests/GameSystem/Dice.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Wetware.GameSystem;
+
+namespace Wetware.Tests.GameSystem;
+
+public class DiceTests
+{
+    [Test]
+    public void IsSeeded_ReturnsCorrectly()
+    {
+        Dice.IsSeeded()
+            .Should()
+            .BeFalse();
+        
+        Dice.Seed(new Random());
+        
+        Dice.IsSeeded()
+            .Should()
+            .BeTrue();
+    }
+
+    [Test]
+    public void Roll_WithBonus_AddsBonus()
+    {
+        const int numOfDice = 1;
+        const int numOfSides = 1;
+        const int expectedResult = 1;
+
+        Dice.Roll(numOfDice, numOfSides)
+            .Should()
+            .Be(expectedResult);
+
+        const int bonus = 1;
+        const int expectedResultWithBonus = expectedResult + bonus;
+        
+        Dice.Roll(numOfDice, numOfSides, bonus)
+            .Should()
+            .Be(expectedResultWithBonus);
+    }
+
+    [Test]
+    public void Roll_Single_Dice_WithinExpectedRange()
+    {
+        const int numOfDice = 1;
+        const int numOfSides = 6;
+        
+        for (var i = 0; i < 1e2; i++)
+            Dice.Roll(numOfDice, numOfSides)
+                .Should()
+                .BeInRange(numOfDice, numOfSides);
+    }
+
+    [Test]
+    public void Roll_MultipleDice_WithinExpectedRange()
+    {
+        const int numberOfDice = 3;
+        const int numberOfSides = 20;
+        
+        for (var i = 0; i < 1e2; i++)
+            Dice.Roll(numberOfDice, numberOfSides)
+                .Should()
+                .BeInRange(numberOfDice, numberOfSides * numberOfDice);
+    }
+}
--- Wetware/GameSystem/Dice.cs
+++ Wetware/GameSystem/Dice.cs
@@ -1,11 +1,9 @@
-﻿using System.Security.Cryptography;
-
-namespace Wetware.GameSystem;
+﻿namespace Wetware.GameSystem;
 
 public static class Dice
 {
-    private static bool _seeded = false;
-    private static Random _random = new Random();
+    private static bool _seeded;
+    private static Random _random = new();
 
     public static void Seed(Random rng)
     {
@@ -15,17 +13,15 @@ public static class Dice
 
     public static bool IsSeeded() => _seeded;
 
-    public static int Roll(int count, int sides)
+    public static int Roll(int count, int sides, int bonus = 0)
     {
-        if (_random is null) throw new InvalidOperationException("Dice.Init() must be called first.");
-
         var total = 0;
         for (var i = 0; i < count; i++)
         {
             total += Throw(sides);
         }
 
-        return total;
+        return total + bonus;
     }
 
     private static int Throw(int sides) => _random.Next(1, sides + 1);
