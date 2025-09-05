c77afb3ba7815664bae928f7a3244da92f9f9b6f
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-05 14:35:31 +0100

    test entities are only initialised if we're starting a new game

--- Wetware/Program.cs
+++ Wetware/Program.cs
@@ -4,15 +4,23 @@ using Wetware.Components;
 using Wetware.Flags;
 using Wetware.Globals;
 
-var game = new Game();
+Console.WriteLine("a name?");
+var name = Console.ReadLine();
 
-game.World.CreateEntity(new UniqueEntity(EId.Clock), new Name(EId.Clock), new Energy(0), new Speed(12), Tags.Get<Clock>());
-game.World.CreateEntity(new Name("reflex machine"), new Energy(0), new Speed(12));
-game.World.CreateEntity(new Name("speed machine"), new Energy(0), new Speed(16));
-game.World.CreateEntity(new Name("slow machine"), new Energy(0), new Speed(6), new Health(20, 10));
+var game = new Game(name);
+
+if (!game.LoadedFromFile) MakeTestEntities();
 
 for (;;)
 {
     Game.Instance.Tick();
     Console.ReadKey(true);
+}
+
+void MakeTestEntities()
+{
+    game.World.CreateEntity(new UniqueEntity(EId.Clock), new Name(EId.Clock), new Energy(0), new Speed(12), Tags.Get<Clock>());
+    game.World.CreateEntity(new Name("reflex machine"), new Energy(0), new Speed(12));
+    game.World.CreateEntity(new Name("speed machine"), new Energy(0), new Speed(16));
+    game.World.CreateEntity(new Name("slow machine"), new Energy(0), new Speed(6), new Health(20, 10));
 }
