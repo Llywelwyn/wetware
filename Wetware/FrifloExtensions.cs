aa263c63c7b76a3c390c73338ef034269906876d
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-05 12:32:00 +0100

    adds DebugName() extension to Entity

---
+++ Wetware/FrifloExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Friflo.Engine.ECS;
+using Wetware.Components;
+
+namespace Wetware;
+
+public static class EntityExtensions
+{
+    /// <summary>
+    /// Attempts to get the name of an entity and it's ID. If the entity has no name, it returns the ID only. 
+    /// </summary>
+    /// <remarks>
+    /// This is explicitly used for debugging. It will not respect unidentified entities, or even if the given entity
+    /// has a name at all.
+    /// </remarks>
+    public static string DebugName(this Entity e)
+    {
+        if (e.TryGetComponent(out Name name))
+        {
+            return $"{name.Value} ({e.Id})";
+        }
+
+        return e.HasName ? $"{e.Name.value} ({e.Id})" : $"unnamed {e.Id}";
+    }
+}
