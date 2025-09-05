b91cf5681ae7d79323ba31116ec29dd7d8300107
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-05 23:15:31 +0100

    adds Map, storing [,] indexes of tiles. inits a PositionChangeListener that keeps index in sync

---
+++ Wetware/Map/Map.cs
@@ -0,0 +1,25 @@
+﻿using Friflo.Engine.ECS;
+using Wetware.Components;
+using Position = Wetware.Components.Position;
+
+namespace Wetware.Map;
+
+public class Map
+{
+    public readonly int Width;
+    public readonly int Height;
+    private float[,] _obstruction;
+    private readonly PositionChangeListener _listener;
+
+    public Map(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        _obstruction = new float[width, height];
+        _listener = new PositionChangeListener(this);
+    }
+    
+    public ref float this[Position pos] => ref _obstruction[pos.X, pos.Y];
+    
+    public bool InBounds(Position pos) => pos.X >= 0 && pos.X < Width && pos.Y >= 0 && pos.Y < Height;
+}
---
+++ Wetware/Map/PositionChangeListener.cs
@@ -0,0 +1,46 @@
+﻿using Friflo.Engine.ECS;
+using Wetware.Components;
+using Position = Wetware.Components.Position;
+
+namespace Wetware.Map;
+
+public class PositionChangeListener
+{
+    private readonly Map _map;
+
+    public PositionChangeListener(Map map) 
+    {
+        _map = map;
+        Subscribe();
+    }
+
+    private void Subscribe()
+    {
+        var world = Game.Instance.World;
+        world.OnComponentAdded += HandleComponentAdded;
+        world.OnComponentRemoved += HandleComponentRemoved;
+        world.OnEntityDelete += HandleEntityDelete;
+    }
+
+    private void HandleComponentAdded(ComponentChanged e)
+    {
+        if (e.Type != typeof(Position)) return;
+        if (!e.Entity.TryGetComponent(out Obstruction obstruction)) return;
+        if (e.Action == ComponentChangedAction.Update) _map[e.OldComponent<Position>()] -= obstruction.Value;
+        _map[e.Component<Position>()] += obstruction.Value;
+    }
+
+    private void HandleComponentRemoved(ComponentChanged e)
+    {
+        if (e.Type != typeof(Position)) return;
+        if (!e.Entity.TryGetComponent(out Obstruction obstruction)) return;
+        _map[e.OldComponent<Position>()] -= obstruction.Value;
+    }
+
+    private void HandleEntityDelete(EntityDelete e)
+    {
+        if (!e.Entity.TryGetComponent(out Position pos)) return;
+        if (!e.Entity.TryGetComponent(out Obstruction obstruction)) return;
+        _map[pos] -= obstruction.Value;
+    }
+}
