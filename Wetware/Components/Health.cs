952bf5de53fd9f07875d8236906cd22dd0f72388
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-05 12:32:17 +0100

    adds Health(int max, int? current) component

---
+++ Wetware/Components/Health.cs
@@ -0,0 +1,9 @@
+﻿using Friflo.Engine.ECS;
+
+namespace Wetware.Components;
+
+public struct Health(int max, int? current = null) : IComponent
+{
+    public readonly int Max = max;
+    public int Current = current ?? max;
+}
