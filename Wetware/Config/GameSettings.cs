ca10923e54a48b0b65cf9d4aff7d2a4c9ae87547
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-05 17:12:26 +0100

    adds Config/GameSettings

---
+++ Wetware/Config/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Wetware.Config;
+
+public static class GameSettings
+{
+   private const string Title = "Wetware";
+   private const string Version = "0.0.1";
+   private const string Author = "Lewis Wynne";
+
+   public static string Attribution() => $"{Title} {Version}, by {Author}.";
+}
