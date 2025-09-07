d4976c83e17f9986983aadb1ac90a42b192c6d00
Lewis Wynne <lewiiswynne@gmail.com>
2025-09-07 21:14:38 +0100

    added Renderable to test entities

--- Wetware/Program.cs
+++ Wetware/Program.cs
@@ -3,6 +3,7 @@ using Wetware;
 using Wetware.Components;
 using Wetware.Flags;
 using Wetware.Globals;
+using Wetware.Assets;
 
 Console.WriteLine("a name?");
 var name = Console.ReadLine();
@@ -11,7 +12,7 @@ var game = new Game(name);
 
 if (!game.LoadedFromFile) MakeTestEntities();
 
-for (;;)
+for (; ; )
 {
     Game.Instance.Tick();
     Console.ReadKey(true);
@@ -19,8 +20,26 @@ for (;;)
 
 void MakeTestEntities()
 {
-    game.World.CreateEntity(new UniqueEntity(EId.Clock), new Name(EId.Clock), new Energy(0), new Speed(12), Tags.Get<Clock>());
-    game.World.CreateEntity(new Name("reflex machine"), new Energy(0), new Speed(12));
-    game.World.CreateEntity(new Name("speed machine"), new Energy(0), new Speed(16));
-    game.World.CreateEntity(new Name("slow machine"), new Energy(0), new Speed(6), new Health(20, 10));
+    game.World.CreateEntity(
+        new UniqueEntity(EId.Clock),
+        new Name(EId.Clock),
+        new Energy(0),
+        new Speed(12),
+        Tags.Get<Clock>());
+    game.World.CreateEntity(
+        new Name("reflex machine"),
+        new Renderable(Sprite.m),
+        new Energy(0),
+        new Speed(12));
+    game.World.CreateEntity(
+        new Name("speed machine"),
+        new Renderable(Sprite.m),
+        new Energy(0),
+        new Speed(16));
+    game.World.CreateEntity(
+        new Name("slow machine"),
+        new Renderable(Sprite.m),
+        new Energy(0),
+        new Speed(6),
+        new Health(20, 10));
 }
