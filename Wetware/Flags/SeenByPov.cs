a15ef050a3e9cfff49eb778c233bbe18fa3f8502
Lewis Wynne <lewiiswynne@gmail.com>
2025-09-07 22:50:03 +0100

    EntityRenderSystem: only renders entities currently Seen by the current POV

---
+++ Wetware/Flags/SeenByPov.cs
@@ -0,0 +1,6 @@
+using Friflo.Engine.ECS;
+
+namespace Wetware.Flags;
+
+[TagName("s")]
+public struct SeenByPov : ITag;
--- Wetware/Systems/EntityRenderSystem.cs
+++ Wetware/Systems/EntityRenderSystem.cs
@@ -1,13 +1,21 @@
 using Friflo.Engine.ECS;
 using Friflo.Engine.ECS.Systems;
 using Wetware.Config;
+using Wetware.Flags;
 using Wetware.Components;
 using Position = Wetware.Components.Position;
 
 namespace Wetware.Systems;
 
+/// <summary>
+/// Queries Renderable entities with a Position. Filters out anything not currently seen by the player,
+/// and renders to the screen.
+/// </summary>
+/// <remarks>RenderSystem - no data should change here, save for Render-specific flags.</remarks>
 public class EntityRenderSystem : QuerySystem<Position, Renderable>
 {
+    public EntityRenderSystem() => Filter.AllTags(Tags.Get<SeenByPov>());
+
     protected override void OnUpdate()
     {
         Debug.SystemBoundaryStart(nameof(EntityRenderSystem));
@@ -16,8 +24,6 @@ public class EntityRenderSystem : QuerySystem<Position, Renderable>
         {
             Render(position.X, position.Y, renderable.Sprite);
         });
-
-        CommandBuffer.Playback();
     }
 
     // TODO: actually render to a ScreenSurface.
