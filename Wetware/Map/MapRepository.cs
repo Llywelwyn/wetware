5518f3a371bfd493f2c74ef4baddf108845d5563
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-08 16:54:01 +0100

    MapRepository: docs

--- Wetware/Map/MapRepository.cs
+++ Wetware/Map/MapRepository.cs
@@ -1,11 +1,27 @@
 namespace Wetware.Map;
 using Components;
 
+/// <summary>
+/// Map storage. Stores all game maps in a Dictionary indexed by their (X, Y) position in the world, as well as the index of the
+/// current map being rendered (determined by the map of the POV character).
+/// </summary>
 public class MapRepository
 {
-    private OnMap _currentMapIndex = new(0, 0);
-    private readonly Dictionary<(int, int), Map> _maps = new();
+    /// <summary>The index of the currently active map. Determined by the POV entity.</summary>
+    private OnMap _currentMapIndex;
 
+    /// <summary>All game maps indexed by position in the world.</summary>
+    private readonly Dictionary<(int, int), Map> _maps;
+
+    public MapRepository()
+    {
+        _currentMapIndex = new(0, 0);
+        _maps = new();
+    }
+
+    /// <summary>Fetches the active map.</summary>
     public Map CurrentMap() => _maps[_currentMapIndex.GetIndexedValue()];
+
+    /// <summary>Changes the active map.</summary>
     public void ChangeMap(OnMap map) => _currentMapIndex = map;
 }
