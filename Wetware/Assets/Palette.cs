2725c4c3b456e4067e9162f8af8b6dd658517bf8
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-09-08 18:43:23 +0100

    added Assets/Palette

---
+++ Wetware/Assets/Palette.cs
@@ -0,0 +1,23 @@
+namespace Wetware.Assets;
+
+public enum Palette
+{
+  DarkRed = "a64a2e",
+  Red = "d74200",
+  DarkOrange = "f15f22",
+  Orange = "e99f10",
+  Brown = "98875f",
+  Gold = "cfc041",
+  DarkGreen = "009403",
+  Green = "00c420",
+  DarkBlue = "0048bd",
+  Blue = "0096ff",
+  DarkCyan = "40a4b9",
+  Cyan = "77bfcf",
+  DarkMagenta = "b154cf",
+  Magenta = "da5bd6",
+  Background = "0f3b3a",
+  DarkGrey = "155352",
+  Grey = "b1c9c3",
+  White = "ffffff"
+}
