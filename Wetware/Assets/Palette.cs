0fc47a6f73d347d9a20a89e017b50da6d75d3ed8
Lewis Wynne <lewiiswynne@gmail.com>
2025-09-08 22:54:39 +0100

    Palette: from Enum to static class props

--- Wetware/Assets/Palette.cs
+++ Wetware/Assets/Palette.cs
@@ -1,23 +1,23 @@
 namespace Wetware.Assets;
 
-public enum Palette
+public static class Palette
 {
-  DarkRed     = "#a64a2e",
-  Red         = "#d74200",
-  DarkOrange  = "#f15f22",
-  Orange      = "#e99f10",
-  Brown       = "#98875f",
-  Gold        = "#cfc041",
-  DarkGreen   = "#009403",
-  Green       = "#00c420",
-  DarkBlue    = "#0048bd",
-  Blue        = "#0096ff",
-  DarkCyan    = "#40a4b9",
-  Cyan        = "#77bfcf",
-  DarkMagenta = "#b154cf",
-  Magenta     = "#da5bd6",
-  Background  = "#0f3b3a",
-  DarkGrey    = "#155352",
-  Grey        = "#b1c9c3",
-  White       = "#ffffff"
+    public const string DarkRed = "#a64a2e";
+    public const string Red = "#d74200";
+    public const string DarkOrange = "#f15f22";
+    public const string Orange = "#e99f10";
+    public const string Brown = "#98875f";
+    public const string Gold = "#cfc041";
+    public const string DarkGreen = "#009403";
+    public const string Green = "#00c420";
+    public const string DarkBlue = "#0048bd";
+    public const string Blue = "#0096ff";
+    public const string DarkCyan = "#40a4b9";
+    public const string Cyan = "#77bfcf";
+    public const string DarkMagenta = "#b154cf";
+    public const string Magenta = "#da5bd6";
+    public const string Background = "#0f3b3a";
+    public const string DarkGrey = "#155352";
+    public const string Grey = "#b1c9c3";
+    public const string White = "#ffffff";
 }
